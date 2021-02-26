05b31949abb6cbc0edd73108b5c4cfb7a227b3af
Luke Phillips <luke.thomas.phillips@gmail.com>
2021-02-26 13:13:17 -0700

    broken material filter

--- Pages/DeskQuotes/Index.cshtml.cs
+++ Pages/DeskQuotes/Index.cshtml.cs
@@ -1,12 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
-using MegaDeskWeb.Data;
 using MegaDeskWeb.Models;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace MegaDeskWeb.Pages.DeskQuotes
 {
@@ -20,12 +20,29 @@ namespace MegaDeskWeb.Pages.DeskQuotes
         }
 
         public IList<DeskQuote> DeskQuotes { get; set; }
-
+        
+        public SelectList Materials { get; set; }
+        
+        [BindProperty(SupportsGet = true)]
+        public string Material { get; set; }
+        
         public async Task OnGetAsync()
         {
             DeskQuotes = await _context.DeskQuote
                 .Include(d => d.Desk)
                 .Include(d => d.RushOrderOption).ToListAsync();
+            
+            if (!string.IsNullOrEmpty(Material))
+            {
+                Console.WriteLine("DeskQuotes is: " + DeskQuotes.ToString());
+                //Console.WriteLine("DeskQuotes is: " + DeskQuotes.ToString());
+                DeskQuotes = DeskQuotes.Where(d => d.Desk.DesktopSurfaceMaterial.Name == Material).ToList();
+            }
+
+            IQueryable<string> materialQuery = from m in _context.DesktopSurfaceMaterial
+                                               select m.Name;
+
+            Materials = new SelectList(await materialQuery.Distinct().ToListAsync());
         }
     }
 }
